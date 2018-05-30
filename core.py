40f64dd82631dee84a99837d6c5a43fecbef4465
bennr01 <benjamin99.vogt@web.de>
2018-05-30 20:45:06 +0200

    set version to 999.999.999

--- core.py
+++ core.py
@@ -5,7 +5,7 @@ StaSh - Pythonista Shell
 https://github.com/ywangd/stash
 """
 
-__version__ = '0.0.1'
+__version__ = '999.999.999'
 
 import imp as pyimp  # rename to avoid name conflict with objc_util
 import logging
